65def4844ed069432ce50df75e0509fa83bb551d
aryn weatherly <arynweatherly93@gmail.com>
2020-02-03 10:08:42 -0600

    added models

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using BackEndCapstone.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,5 +14,47 @@ namespace BackEndCapstone.Data
             : base(options)
         {
         }
-    }
+        public DbSet<Product> Product { get; set; }
+        public DbSet<Gemstone> Gemstone { get; set; }
+        public DbSet<ProductType> ProductType { get; set; }
+        public DbSet<Tutorial> Tutorial { get; set; }
+        public DbSet<TutorialReview> TutorialReview { get; set; }
+        public DbSet<ProductReview> ProductReview { get; set; }
+        public DbSet<ApplicationUser> ApplicationUser { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+
+            ApplicationUser user = new ApplicationUser
+            {
+                Boom = "KHBOOM",
+                FirstName = "admin",
+                LastName = "admin",
+                UserName = "admin@admin.com",
+                NormalizedUserName = "ADMIN@ADMIN.COM",
+                Email = "admin@admin.com",
+                NormalizedEmail = "ADMIN@ADMIN.COM",
+                EmailConfirmed = true,
+                LockoutEnabled = false,
+                SecurityStamp = "7f434309-a4d9-48e9-9ebb-8803db794577",
+                Id = "00000000-ffff-ffff-ffff-ffffffffffff"
+            };
+            var passwordHash = new PasswordHasher<ApplicationUser>();
+            user.PasswordHash = passwordHash.HashPassword(user, "Admin8*");
+            modelBuilder.Entity<ApplicationUser>().HasData(user);
+            Cat maisey = new Cat()
+            {
+                Id = 1,
+                Name = "Maisey",
+                Breed = "cranky",
+                Gender = "female",
+                IsChonky = true,
+                IsFixed = true
+
+            };
+            modelBuilder.Entity<Cat>().HasData(maisey);
+        }
 }
